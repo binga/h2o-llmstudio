68434a5cfa708009df028060ecb22f9e8ac331ae
maxjeblick <maximilianjeblick@gmail.com>
2023-07-19 14:46:45 +0200

    rename method name

--- llm_studio/src/utils/config_utils.py
+++ llm_studio/src/utils/config_utils.py
@@ -212,4 +212,4 @@ def load_config_yaml(path: str):
         problem_type = cfg_dict["problem_type"]
         raise NotImplementedError(f"Problem Type {problem_type} not implemented")
 
-    return ConfigProblemBase.from_config_dict(cfg_dict)
+    return ConfigProblemBase.from_config(cfg_dict)
